6d8016addc4bab125e24f3cc7b3494c5a734b44c
Nicholas Koh <nkohwl@gmail.com>
2018-06-22 23:02:10 +0800

    Add dictionary and script to precheck words

---
+++ validate.rb
@@ -0,0 +1,11 @@
+require 'json'
+
+require "./helpers"
+
+
+dictionary = File.read("dictionary.txt").split("\n")
+board = File.read("board.txt").split(", ")
+
+# pre-check words that can be formed on the board
+dictionary = dictionary.select {|word| check_word(board, word)}
+File.write("validated_dictionary.json", dictionary.to_json)
