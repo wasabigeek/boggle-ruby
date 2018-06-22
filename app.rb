97b04a41fe552765daa186a0a675c7a044546965
Nicholas Koh <nkohwl@gmail.com>
2018-06-22 23:24:04 +0800

    Add printing of the board

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ require "json"
 valid_words = JSON.parse(File.read('valid_words.json'))
 puts "How to play? Enter a word, or press CTRL-C to exit."
 
+puts "Here's the board:"
+board = File.read('board.txt').split(", ")
+board.each_with_index {|tile, index|
+    print "#{tile} #{"\n"if index % 4 == 3}"
+}
+
 score = 0
 loop do
     puts "Your current score is #{score}. Enter a word:"
